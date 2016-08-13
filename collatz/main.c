1c6e90140d37c8a1d5d811a183822f9ccad05505
jkanschik <jens_kanschik@freenet.de>
2016-08-13 14:19:46 +0200

    Initial Commit

---
+++ collatz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  collatz
+//
+//  Created by Jens Kanschik on 13.08.16.
+//  Copyright © 2016 Jens Kanschik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
